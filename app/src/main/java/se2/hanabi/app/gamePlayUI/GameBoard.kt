296ee37a59dd081f8944bbd83b8366a4b1582e9b
Jack Currie <jackcurrie2310@gmail.com>
2025-04-19 09:47:32 +0200

    Prepared layout of gameBoard - Tokens area defined so that left and right columns height match - GameBoardUI Row allowed to take size of content. background on this row indicates a table/actual gamebaord.

--- app/src/main/java/se2/hanabi/app/gamePlayUI/GameBoard.kt
+++ app/src/main/java/se2/hanabi/app/gamePlayUI/GameBoard.kt
@@ -1,16 +1,19 @@
 package se2.hanabi.app.gamePlayUI
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.TextStyle
@@ -20,6 +23,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import se2.hanabi.app.card.Card
 import se2.hanabi.app.card.CardItem
+import se2.hanabi.app.card.cardHeight
+import se2.hanabi.app.card.cardWidth
 
 /*
 displays common game board elements such as: fuse/hint tokens, discard/draw pile, color stacks
@@ -31,23 +36,60 @@ fun GameBoardUI(
     lastDiscardedCard: Card
     ) {
     Row(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .clip(RoundedCornerShape(10.dp))
+            .background(Color(0xFF566290).copy(alpha = 0.5f)),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center
     ) {
-        Column(
-            modifier = Modifier.padding(10.dp),
-            verticalArrangement = Arrangement.spacedBy(5.dp)
+        Column( //left colomn
+            modifier = Modifier
+//                .background(Color.Green.copy(alpha = 0.5f)) // for testing
+                .padding(boardElementPadding),
+            verticalArrangement = Arrangement.spacedBy(boardElementPadding)
 
         ) {
-            //FuseTokens()
-            RemainingCardsStack(numRemainingCards = numRemainingCards)
-            DiscardedCardsStack(lastDiscardedCard = lastDiscardedCard)
-            //HintTokens()
+            FuseTokens(modifier = Modifier) // .weight(1f)
+            Column(
+//                modifier = Modifier
+//                    .background(Color.White.copy(alpha = 0.5f)), // for testing
+                verticalArrangement = Arrangement.spacedBy(cardSpacing)
+
+            ) {
+                RemainingCardsStack(numRemainingCards = numRemainingCards)
+                DiscardedCardsStack(lastDiscardedCard = lastDiscardedCard)
+            }
+            HintTokens()
         }
+        // right column
         ColorStacks(stackValues = stackValues)
     }
 }
+
+val cardSpacing = 5.dp
+val boardElementPadding = 10.dp
+val tokenAreaHeight = ( cardWidth.times(3) + cardSpacing.times(3) - boardElementPadding.times(2) ).div(2)
+
+@Composable
+fun HintTokens() {
+    Box(modifier = Modifier
+        .size(cardHeight, tokenAreaHeight)
+//        .background(Color.Red.copy(alpha = 0.5f)) // for testing
+        ) {
+        //TODO show Tokens
+    }
+}
+
+@Composable
+fun FuseTokens(modifier: Modifier) {
+    Box(modifier = modifier
+        .size(cardHeight, tokenAreaHeight)
+//        .background(Color.Red.copy(alpha = 0.5f)) // for testing
+    ) {
+        //TODO show Tokens
+    }
+}
+
 @Composable
 fun RemainingCardsStack(
     modifier: Modifier = Modifier,
@@ -107,8 +149,10 @@ fun ColorStacks(
     stackValues: IntArray,
 ) {
     Column(
-        verticalArrangement = Arrangement.spacedBy(5.dp),
-        modifier = Modifier.padding(10.dp)
+        modifier = Modifier
+//            .background(Color.White.copy(alpha = 0.5f)) // for testing
+            .padding(boardElementPadding),
+        verticalArrangement = Arrangement.spacedBy(cardSpacing),
     ) {
         colors.forEachIndexed() {index, color ->
             if (stackValues[index]==0) {
--- app/src/main/java/se2/hanabi/app/gamePlayUI/GamePlayUI.kt
+++ app/src/main/java/se2/hanabi/app/gamePlayUI/GamePlayUI.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -28,7 +29,8 @@ fun GamePlayUI() {
             Brush.verticalGradient(
                 listOf(Color(0xFF282828), Color(0xFF000000))
             )
-        )
+        ),
+        contentAlignment = Alignment.Center
     ) {
         val hands = generateTestHands(5)
         PlayersCardsUI(hands)
