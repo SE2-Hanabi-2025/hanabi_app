09ac3e1b85878df35ddf611c499790ec1b049d56
enakecanovic <enkecanovic@edu.aau.at>
2025-04-25 01:11:49 +0200

    Player list adaptation

--- app/src/main/java/se2/hanabi/app/lobby/LobbyActivity.kt
+++ app/src/main/java/se2/hanabi/app/lobby/LobbyActivity.kt
@@ -7,23 +7,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -53,46 +54,45 @@ fun LobbyScreen (playerList: List<String>, onLeaveLobby: () -> Unit){
             contentDescription = null,
             modifier = Modifier.fillMaxSize(),
             contentScale = ContentScale.Crop)
-    }
-    Column (modifier = Modifier.fillMaxSize()){
-        Box(modifier = Modifier.fillMaxWidth().height(56.dp).background(Color.DarkGray), contentAlignment = Alignment.CenterStart)
+
+    //TODO: change this I dont like it
+        Box(modifier = Modifier.fillMaxWidth().height(56.dp).background(Color.DarkGray), contentAlignment = Alignment.TopCenter)
                { Text (
             text = "Lobby",
             color = Color.White,
-            modifier = Modifier.align(Alignment.CenterStart).padding(start = 16.dp)
-        )  }
-            Column (
-                modifier = Modifier.fillMaxSize().padding(16.dp),
-                verticalArrangement = Arrangement.SpaceBetween,
-                horizontalAlignment = Alignment.CenterHorizontally){
+                   modifier = Modifier.padding(start = 16.dp)
+        )}
 
                 //Player list Placeholder
-
+                Box(modifier = Modifier.fillMaxWidth(0.8f).heightIn(max = 400.dp).offset(y = 250.dp, x = 42.dp).clip(
+                    RoundedCornerShape(40.dp))
+                    .background(Color.Black.copy(alpha = 0.6f))
+                    .padding(40.dp))
+                {
                  LazyColumn (
-                 modifier = Modifier.fillMaxWidth().weight(1f)
+                 modifier = Modifier.fillMaxWidth()
               ){
                     items(playerList) { player ->
                         Text(text = player,
-                            modifier = Modifier.fillMaxWidth().padding(8.dp))
+                            color = Color.White,
+                            modifier = Modifier.fillMaxWidth().padding(vertical = 10.dp))
                 }
             }
-
+           }
                 //leave Lobby button
 
         Button(
             onClick = onLeaveLobby,
             colors = ButtonDefaults.buttonColors(
-           containerColor = Color.DarkGray
-
-            ),
+           containerColor = Color.DarkGray),
             border = BorderStroke(2.dp, Color.White),
-            modifier = Modifier.padding(top = 16.dp).width(200.dp).height(60.dp)
+            modifier = Modifier.align(Alignment.BottomCenter).padding(bottom = 32.dp).width(200.dp).height(60.dp)
         ) {
             Text("Leave Lobby", color = Color.White)
         }
-     }
+
     }
-    }
+}
 
 
 @Preview(showBackground = true)
