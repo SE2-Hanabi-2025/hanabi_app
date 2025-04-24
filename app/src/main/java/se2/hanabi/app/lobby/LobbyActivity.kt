97dae9c982b4eea03e0c2ce4a8a6249e0232f5f7
enakecanovic <enkecanovic@edu.aau.at>
2025-04-25 01:43:26 +0200

    fix(ui)lobby: prepare space for avatars in player list and fix alignment

--- app/src/main/java/se2/hanabi/app/lobby/LobbyActivity.kt
+++ app/src/main/java/se2/hanabi/app/lobby/LobbyActivity.kt
@@ -7,16 +7,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -28,6 +32,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import se2.hanabi.app.ui.theme.ClientTheme
@@ -67,16 +72,27 @@ fun LobbyScreen (playerList: List<String>, onLeaveLobby: () -> Unit){
                 Box(modifier = Modifier.fillMaxWidth(0.8f).heightIn(max = 400.dp).offset(y = 250.dp, x = 42.dp).clip(
                     RoundedCornerShape(40.dp))
                     .background(Color.Black.copy(alpha = 0.6f))
-                    .padding(40.dp))
+                    .padding(horizontal = 30.dp, vertical = 10.dp))
                 {
                  LazyColumn (
                  modifier = Modifier.fillMaxWidth()
               ){
                     items(playerList) { player ->
-                        Text(text = player,
+                        Row (modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(20.dp))
+                        {
+                            Box(
+                                modifier = Modifier.size(40.dp).clip(CircleShape).background(Color.DarkGray)
+                            )
+
+                            Text(
+                            text = player,
                             color = Color.White,
-                            modifier = Modifier.fillMaxWidth().padding(vertical = 10.dp))
-                }
+                                modifier = Modifier.weight(1f),
+                                textAlign = TextAlign.Start
+                        )
+                        }}
             }
            }
                 //leave Lobby button
