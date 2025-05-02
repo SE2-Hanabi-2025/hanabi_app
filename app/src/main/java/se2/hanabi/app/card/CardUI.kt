584821a6f1ceeec4d7314956e974e051749c90a6
Jack Currie <jackcurrie2310@gmail.com>
2025-05-02 09:55:23 +0200

    Added Color and Value hint layer to CardUI - Hint indicated on top left corner of card - When value hint is given and no color hint is given then the value is shown as white text on a black gradient - when the color hint is given then the gradient takes the color of the hint and the value is black.

--- app/src/main/java/se2/hanabi/app/card/CardUI.kt
+++ app/src/main/java/se2/hanabi/app/card/CardUI.kt
@@ -1,23 +1,32 @@
 package se2.hanabi.app.card
 
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import se2.hanabi.app.gamePlayUI.BackGlow
+import se2.hanabi.app.gamePlayUI.colorFromString
 
 const val aspectRatio = 1.51f
 val cardWidth = 66.dp
@@ -48,23 +57,17 @@ fun CardItem(
     isSelected: Boolean = false,
     isHighlighted: Boolean = isSelected,
     onClick: () -> Unit = {},
-    highlightColor: Color = Color.White
+    highlightColor: Color = Color.White,
+    showColorHint: Boolean = true,
+    showValueHint: Boolean = true,
 ) {
-    val rotationAmountY by animateFloatAsState(
-        targetValue = if (isFlipped) 180f else 0f,
-        label = "cardFlip"  // for debugging
-    ) // animation progress, where 0f (front), 180f (back)
-
     var actualCardWidth = if (isPortrait) cardWidth else cardHeight
     var actualCardHeight = if (isPortrait) cardHeight else cardWidth
-
-
 
     Box(
         modifier = modifier
             .size(actualCardWidth, actualCardHeight )
             .graphicsLayer {
-                rotationY = rotationAmountY
                 rotationZ = rotationAmountZ
                 cameraDistance = 12f * density
             }
@@ -83,7 +86,7 @@ fun CardItem(
             )
         }
 
-        val imageName = if(rotationAmountY <= 90f) getCardImageName(card) else "card_backside"
+        val imageName = if(!isFlipped) getCardImageName(card) else "card_backside"
         val imageID = LocalContext.current.resources.getIdentifier(
             imageName, "drawable", LocalContext.current.packageName
         )
@@ -99,5 +102,34 @@ fun CardItem(
             contentDescription = "Front side: $imageName image",
             contentScale = ContentScale.Fit
         )
+        //hint overlay
+        if (showColorHint || showValueHint) {
+            val cardHintBackgroundColor = if (showColorHint) colorFromString(card.color) else Color.Black
+            val cardHintValueColor = if (showColorHint) Color.Black else Color(0xFFF2FF90)
+            Box(
+                modifier = Modifier
+                    .matchParentSize()
+                    .background(
+                        brush = Brush.radialGradient(
+                            colors = listOf(cardHintBackgroundColor, cardHintBackgroundColor.copy(alpha = 0f)),
+                            center = Offset(0f, 0f),
+                            radius = with(LocalDensity.current) {actualCardWidth.toPx()*0.7f }
+                        )
+                    ),
+                contentAlignment = Alignment.TopStart
+            )   {
+                if (showValueHint) {
+                    Text(
+                        modifier = Modifier.padding(3.dp),
+                        text = "${card.number}",
+                        fontFamily = FontFamily.Cursive,
+                        color = cardHintValueColor,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+
+            }
+        }
     }
 }
