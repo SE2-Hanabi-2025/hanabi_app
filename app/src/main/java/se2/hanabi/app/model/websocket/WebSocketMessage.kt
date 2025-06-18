9d4178a07ff440bd0281da0c5730d20dc7168a8c
thedovla <donjakvlado@gmail.com>
2025-06-18 14:46:47 +0200

    Update WebSocketMessage.kt
    Add CheatAction data class for cheat detection WebSocket messages

--- app/src/main/java/se2/hanabi/app/model/websocket/WebSocketMessage.kt
+++ app/src/main/java/se2/hanabi/app/model/websocket/WebSocketMessage.kt
@@ -54,6 +54,15 @@ data class GiveHintAction(
 }
 
 @Serializable
+@SerialName("CHEAT")
+data class CheatAction(
+    override val lobbyId: String,
+    override val playerId: Int
+) : ClientAction() {
+    override val action: String = "CHEAT"
+}
+
+@Serializable
 enum class HintType {
     COLOR,
     VALUE;
